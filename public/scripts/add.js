0d6cac121cde9f58d819b64940e18a9506b4d08b
Anthony <quaz711@hotmail.com>
2023-03-03 15:30:30 -0500

    created add.js file for public

---
+++ public/scripts/add.js
@@ -0,0 +1,27 @@
+async function newForm(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const postText = document.querySelector('textarea[name="post-text"]').value;
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            postText
+        }),
+
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    }
+
+    else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newForm);
